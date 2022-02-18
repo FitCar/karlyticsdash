e8fb333801c924731ac0724df76624427f4d05e4
Ebireni David <ebirenidavid@gmail.com>
2022-02-18 14:35:58 +0100

    refactored code to add new quote and change the values in the quote

--- src/components/quotation/Quotation.jsx
+++ src/components/quotation/Quotation.jsx
@@ -6,38 +6,29 @@ import firebase from "firebase";
 const firestore = firebase.firestore();
 
 export default function Quotation() {
-  const [item, setItem] = useState([]);
-  const [quote, setQuote] = useState([]);
+  const [quotes, setQuotes] = useState([
+    { index: 1, partNumber: "", description: "", qty: 0, unitPrice: 0, total: 0  }
+  ]);
+  
   const id = useParams();
   const requestDoc = id.requestId;
   const customerDoc = id.customerId;
 
-  let pan = {};
-
-  const runPan = (name, value) => {
-    console.log(name);
-    console.log(value);
-    pan[name] = value;
-    console.log(pan);
-  };
-
   const addItem = (e) => {
     e.preventDefault();
-    setItem([...item, pan]);
-    pan = {};
-    e.target.reset();
+    setQuotes([ ...quotes, { index: quotes.length+1, partNumber: "", description: "", qty: 0, unitPrice: 0, total: 0  } ])
   };
 
-  console.log(item);
-  console.log(pan);
+  
+  const setItem = (e, num) => {
+    const changedQuote = quotes.map(item => {
+      return item.index === num ? { ...item, [e.target.name]: e.target.value } : { ...item }
+    })
 
-  const setItems = (e) => {
-    console.log(e.target.value);
-    console.log(e.target.name);
-    let name = e.target.name;
-    let value = e.target.value;
-    runPan(name, value);
+    return setQuotes(changedQuote)
   };
+
+  console.log(quotes)
 
   const sendQuotation = () => {
     const quotationRef = firestore
@@ -46,7 +37,7 @@ export default function Quotation() {
     .collection("Quotation")
     .doc(requestDoc);
 
-    quotationRef.set({item});
+    quotationRef.set(quotes);
   }
 
   return (
@@ -54,57 +45,24 @@ export default function Quotation() {
       <h3>Quotation</h3>
 
       <div className="quotationItem">
+        
         <form onSubmit={addItem}>
-          <input
-            placeholder="Part number"
-            type="text"
-            name="partNumber"
-            onChange={setItems}
-          />
-          <input
-            placeholder="Description"
-            type="text"
-            name="description"
-            onChange={setItems}
-          />
-          <input
-            placeholder="Qty"
-            type="number"
-            name="qty"
-            onChange={setItems}
-          />
-          <input
-            placeholder="Unit Price"
-            type="text"
-            name="unitPrice"
-            onChange={setItems}
-          />
-          <input
-            placeholder="Total"
-            type="text"
-            name="total"
-            onChange={setItems}
-          />
-          <input type="submit" value="Add" />
-        </form>
-        <div>
-          {item.map((items) => {
+          <input type="submit" value="Add new Cell" />
+
+          {quotes.map((item) => {
             return (
               <div className="quotationList">
-                <input type="text" value={items.partNumber} />
-                <input type="text" value={items.description} />
-                <input type="text" value={items.qty} />
-                <input type="text" value={items.unitPrice} />
-                <input type="text" value={items.total} />
+                <input type="text" value={item.partNumber} name="partNumber" onChange={(e) => setItem(e, item.index)} />
+                <input type="text" value={item.description} name="description" onChange={(e) => setItem(e, item.index)} />
+                <input type="number" value={item.qty} name="qty" onChange={(e) => setItem(e, item.index)} />
+                <input type="number" value={item.unitPrice} name="unitPrice" onChange={(e) => setItem(e, item.index)} />
+                <input type="number" value={item.total} name="total" onChange={(e) => setItem(e, item.index)} />
               </div>
             );
           })}
-        </div>
-      </div>
 
-      {/* <div>
-        <button onClick={addItem}>Add Item</button>
-      </div> */}
+        </form>
+      </div>
 
       <div className="quotationSend">
         <button onClick={sendQuotation}>Send Quotation</button>
